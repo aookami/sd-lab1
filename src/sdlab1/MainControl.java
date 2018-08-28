52081c9dd5f0401ba463238733bf5d3a6f53e39a
a1501895 <a1501895@rle-desktop>
2018-08-28 15:06:09 -0300

    asdf

--- src/sdlab1/MainControl.java
+++ src/sdlab1/MainControl.java
@@ -1,10 +1,71 @@
 package sdlab1;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.UnknownHostException;
+
 public class MainControl {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static Thread t0 = new Thread("MC") {
+		@Override
+		public void run() {
+			MulticastSocket socket;
+			InetAddress group;
 
+			try {
+				group = InetAddress.getByName("224.24.24.42");
+				socket = new MulticastSocket(4446);
+				socket.joinGroup(group);
+				listenToMc(socket, group);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+	};
+
+	static Thread t1 = new Thread("Vash") {
+		@Override
+		public void run() {
+			Participant t1 = new Participant(113);
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			t1.enterRoom();
+			t1.exitRoom();
+
+		}
+	};
+
+	public static void main(String[] args) {
+
+			t0.start();
+
+			t1.start();
+
+	}
+
+	static void listenToMc(MulticastSocket socket, InetAddress group) {
+		while (true) {
+			byte[] buf = new byte[256];
+			DatagramPacket pkct = new DatagramPacket(buf, buf.length);
+			try {
+				socket.receive(pkct);
+			} catch (IOException e) {
+				System.out.println("IOException at listenToMc");
+
+			}
+
+			String received = new String(pkct.getData());
+			System.out.println("Main received: " + received);
+
+		}
 	}
 
 }
--- src/sdlab1/Participant.java
+++ src/sdlab1/Participant.java
@@ -1,34 +1,106 @@
 package sdlab1;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 
 public class Participant {
-	
-	
-	String name;
-	
-	private int privatekey;
-	
+
+	int name;
+
+	private String privatekey;
+
 	InetAddress aHost;
-	
-	public int publickey;
-	
+
+	public String publickey;
+
 	MulticastSocket msckt;
-	Participant(String name){
-		privatekey = Integer.valueOf(name)*Integer.valueOf(name)%50000;
-		publickey = Integer.valueOf(name)*2%50000;
-		
-		try {
-			msckt = new MulticastSocket();
-			aHost = InetAddress.getByName("127.0.0.1");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	Thread listener = new Thread(){
+		@Override
+		public void run(){
+			MulticastSocket socket;
+			InetAddress group;
+
+			try {
+				group = InetAddress.getByName("224.24.24.42");
+				socket = new MulticastSocket(4446);
+				socket.joinGroup(group);
+				listenToMc(socket, group);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
-		
-		
+	};
+	
+	Participant(int name) {
+		this.name = name;
+		privatekey = String.valueOf(Integer.valueOf(name) * Integer.valueOf(name) % 50000);
+		publickey = String.valueOf(Integer.valueOf(name) * 2 % 50000);
+
+		try {
+			msckt = new MulticastSocket(4446);
+			aHost = InetAddress.getByName("224.24.24.42");
+			msckt.joinGroup(aHost);
+		} catch (IOException e) {
+			System.out.println("IOException at " + name + " init");
+		}
+		listener.start();
 	}
 
+	void enterRoom() {
+
+		String a = String.valueOf(name);
+		String b = "ENTER";
+
+		String sc = a + ":" + b + ":" + publickey;
+		byte[] c = sc.getBytes();
+		try {
+		
+			DatagramPacket p = new DatagramPacket(c, c.length, aHost, 4446);
+
+			msckt.send(p);
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage() + "AAAAAAAa");
+		}
+
+	}
+
+	void exitRoom() {
+		String a = String.valueOf(name);
+		String b = "EXIT";
+		String sc = a + ":" + b;
+		byte[] c = sc.getBytes();
+		
+		DatagramPacket p = new DatagramPacket(c, c.length, aHost, 4446);
+		try {
+			msckt.send(p);
+		} catch (IOException e) {
+			System.out.println("IOException at " + name + " send enter msg	");
+		}
+	}
+
+	void listenToMc(MulticastSocket socket, InetAddress group) {
+		while (true) {
+			byte[] buf = new byte[256];
+			DatagramPacket pkct = new DatagramPacket(buf, buf.length);
+			try {
+				socket.receive(pkct);
+				
+			} catch (IOException e) {
+				System.out.println("IOException at listenToMc");
+
+			}
+
+			String received = new String(pkct.getData());
+			
+			if(received.split(":")[0].equals(String.valueOf(name))){
+				continue;
+			}
+			System.out.println(String.valueOf(name)  + " received: " + received);
+
+		}
+	}
 }
